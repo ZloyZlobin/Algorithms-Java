b6a867f815146f8c9e95961efd079655dcd3d133
azlobin <T19817321eSt!@#$1>
2019-06-08 19:27:42 +0700

    Add comparator to mergesort

--- src/Sorting/Merge.java
+++ src/Sorting/Merge.java
@@ -1,13 +1,14 @@
 package Sorting;
 
+import java.util.Comparator;
 import java.util.Random;
 
 public class Merge {
 
-    private static void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi)
+    private static void merge(Object[] a, Object[] aux, int lo, int mid, int hi, Comparator comparator)
     {
-        assert isSorted(a, lo, mid);
-        assert isSorted(a, mid+1, hi);
+        assert isSorted(a, lo, mid, comparator);
+        assert isSorted(a, mid+1, hi, comparator);
 
         int i = lo;
         int j = mid + 1;
@@ -15,47 +16,52 @@ public class Merge {
         {
             if(i > mid) aux[k] = a[j++];
             else if(j > hi) aux[k] = a[i++];
-            else if(less(a[j], a[i])) aux[k] = a[j++];
+            else if(less(a[j], a[i], comparator)) aux[k] = a[j++];
             else aux[k] = a[i++];
         }
 
-        assert isSorted(aux, lo, hi);
+        assert isSorted(aux, lo, hi, comparator);
     }
 
-    private static void sort(Comparable[] a, Comparable[] aux, int lo, int hi)
+    private static void sort(Object[] a, Object[] aux, int lo, int hi, Comparator comparator)
     {
         if(hi <= lo) return;
         int mid = lo + (hi - lo)/2;
-        sort(aux, a, lo, mid);
-        sort(aux, a, mid + 1, hi);
-        if(!less(a[mid + 1], a[mid]))
+        sort(aux, a, lo, mid, comparator);
+        sort(aux, a, mid + 1, hi, comparator);
+        if(!less(a[mid + 1], a[mid], comparator))
         {
             System.arraycopy(a, lo, aux, lo, hi - lo + 1);
             return;
         }
-        merge(a, aux, lo, mid, hi);
+        merge(a, aux, lo, mid, hi, comparator);
     }
 
-    private static boolean less(Comparable a, Comparable b)
+    private static boolean less(Object a, Object b, Comparator comparator)
     {
-        return a.compareTo(b) < 0;
+        return comparator == null ? ((Comparable)a).compareTo(b) < 0 : comparator.compare(a, b) < 0;
     }
 
-    private static boolean isSorted(Comparable[] a) {
-        return isSorted(a, 0, a.length - 1);
+    private static boolean isSorted(Object[] a, Comparator comparator) {
+        return isSorted(a, 0, a.length - 1, comparator);
     }
 
-    private static boolean isSorted(Comparable[] a, int lo, int hi) {
+    private static boolean isSorted(Object[] a, int lo, int hi, Comparator comparator) {
         for (int i = lo + 1; i <= hi; i++)
-            if (less(a[i], a[i-1])) return false;
+            if (less(a[i], a[i-1], comparator)) return false;
         return true;
     }
 
     public static void sort(Comparable[] a)
     {
-        Comparable[] aux = a.clone();
-        sort(aux, a, 0, a.length - 1);
-        assert isSorted(a);
+        sort(a, null);
+    }
+
+    public static void sort(Object[] a, Comparator  comparator)
+    {
+        Object[] aux = a.clone();
+        sort(aux, a, 0, a.length - 1, comparator);
+        assert isSorted(a, comparator);
     }
 
     public static void main(String[] args)
--- src/Sorting/MergeBU.java
+++ src/Sorting/MergeBU.java
@@ -1,12 +1,16 @@
 package Sorting;
 
+import Stack.ArrayStack;
+
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Random;
 
 public class MergeBU {
-    private static void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi)
+    private static void merge(Object[] a, Object[] aux, int lo, int mid, int hi, Comparator comparator)
     {
-        assert isSorted(a, lo, mid);
-        assert isSorted(a, mid+1, hi);
+        assert isSorted(a, lo, mid, comparator);
+        assert isSorted(a, mid+1, hi, comparator);
 
         System.arraycopy(a, lo, aux, lo, hi - lo + 1);
 
@@ -16,39 +20,44 @@ public class MergeBU {
         {
             if(i > mid) a[k] = aux[j++];
             else if(j > hi) a[k] = aux[i++];
-            else if(less(aux[j], aux[i])) a[k] = aux[j++];
+            else if(less(aux[j], aux[i], comparator)) a[k] = aux[j++];
             else a[k] = aux[i++];
         }
 
-        assert isSorted(a, lo, hi);
+        assert isSorted(a, lo, hi, comparator);
     }
 
-    private static boolean isSorted(Comparable[] a, int lo, int hi) {
+    private static boolean isSorted(Object[] a, int lo, int hi, Comparator comparator) {
         for (int i = lo + 1; i <= hi; i++)
-            if (less(a[i], a[i-1])) return false;
+            if (less(a[i], a[i-1], comparator)) return false;
         return true;
     }
 
-    private static boolean isSorted(Comparable[] a) {
-        return isSorted(a, 0, a.length - 1);
+    private static boolean isSorted(Object[] a, Comparator comparator) {
+        return isSorted(a, 0, a.length - 1, comparator);
     }
 
-    private static boolean less(Comparable a, Comparable b)
+    private static boolean less(Object a, Object b, Comparator comparator)
     {
-        return a.compareTo(b) < 0;
+        return comparator == null ? ((Comparable)a).compareTo(b) < 0 : comparator.compare(a, b) < 0;
     }
 
     public static void sort(Comparable[] a)
     {
-        Comparable[] aux = a.clone();
+        sort(a, null);
+    }
+
+    public static void sort(Object[] a, Comparator comparator)
+    {
+        Object[] aux = a.clone();
         for(int sz = 1; sz < a.length; sz += sz)
         {
             for(int lo = 0; lo < a.length - sz; lo += sz + sz)
             {
-                merge(a, aux, lo, lo + sz - 1, Math.min(lo + sz + sz - 1, a.length - 1));
+                merge(a, aux, lo, lo + sz - 1, Math.min(lo + sz + sz - 1, a.length - 1), comparator);
             }
         }
-        assert isSorted(a);
+        assert isSorted(a, comparator);
     }
 
     public static void main(String[] args)
